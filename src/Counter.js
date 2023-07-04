bf7735fb9bffa4911d6df8a5f879a79409a860bd
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-07-03 19:59:45 -0700

    refactor counter to use useReducer hook

--- src/Counter.js
+++ src/Counter.js
@@ -21,17 +21,21 @@ function reducer(state, action) {
 
 
 function Counter() {
-const [counter, setCounter] = useState(0);
+// Here we've replaced the useState hook originally used for counter state.
+const [state, dispatch] = useReducer(reducer, initialState);
 const [hidden, setHidden] = useState(false);
 
 useEffect(() => {
-  document.title = counter;
-}, [counter]);
+  // Now we need to access state.counter to get the counter value.
+  document.title = state.counter;
+}, [state.counter]);
 
   return(
   <>
-   {hidden ? <h1>Count Hidden</h1> : <h1>{counter}</h1>}
-      <button onClick={() => setCounter(counter + 1)}>Count!</button>
+  {/* Same here: we need to access state.counter to get the counter value. */}
+   {hidden ? <h1>Count Hidden</h1> : <h1>{state.counter}</h1>}
+      {/* Now we use dispatch() to send an action to our reducer to update state. */}
+      <button onClick={() => dispatch({type: 'increment'})}>Count!</button>
       <button onClick={() => setHidden(!hidden)}>Hide/Show</button>
   </>
   );
